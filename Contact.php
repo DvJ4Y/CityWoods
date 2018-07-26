16d83b6605a0026f282d316e6a6a9cfd71504276
Digvijay Singh <30365439+DvJ4Y@users.noreply.github.com>
2018-07-27 00:24:21 +0530

    Add files via upload

---
+++ Contact.php
@@ -0,0 +1,317 @@
+
+ 
+ 
+ 
+ 
+ <!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="description" content="We are actively involved in tree plantation to increase the green cover. We have been planting trees in Bangalore since years. This is just a small step to save the oxygen factories around us. These tree plantation drives in Bangalore spreads awareness among citizens and makes them understand the importance of trees around us." />
+<meta name="keywords" content="tree plant, plant tree, save tree, plant tree in bangalore, save tree in bangalore, tree plantation in bangalore, tree plantatin in india, save trees india, save trees, bangalore tree, tree planting group,  plant tree in Hyderabad, save tree in Hyderabad, tree plantation in Hyderabad, Hyderabad tree, Hyderabad tree planting,save tree in delhi, tree plantation in delhi, delhi tree, delhi tree planting,save tree in pune, tree plantation in pune, pune tree, pune tree planting,save tree in chennai, tree plantation in chennai, chennai tree, chennai tree planting" />
+<title>Say Trees</title>
+<link rel="shortcut icon" href="http://www.saytrees.org/images/icons/tabIcon.png"/> 
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/theme.css" rel="stylesheet">
+    <link href="css/bootstrap-reset.css" rel="stylesheet">
+    <!-- <link href="css/bootstrap.min.css" rel="stylesheet">-->
+
+    <!--external css-->
+    <link href="assets/font-awesome/css/font-awesome.css" rel="stylesheet" />
+    <link rel="stylesheet" href="css/flexslider.css"/>
+    <link href="assets/bxslider/jquery.bxslider.css" rel="stylesheet" />
+    <link rel="stylesheet" href="css/animate.css">
+    <link rel="stylesheet" href="assets/owlcarousel/owl.carousel.css">
+    <link rel="stylesheet" href="assets/owlcarousel/owl.theme.css">
+
+    <link href="css/superfish.css" rel="stylesheet" media="screen">
+    <link href='http://fonts.googleapis.com/css?family=Lato' rel='stylesheet' type='text/css'>
+    
+
+    <!-- Custom styles for this template -->
+    <link rel="stylesheet" type="text/css" href="css/component.css">
+    <link href="css/style.css" rel="stylesheet">
+    <link href="css/style-responsive.css" rel="stylesheet" />
+
+    <link rel="stylesheet" type="text/css" href="css/parallax-slider/parallax-slider.css" />
+    <script type="text/javascript" src="js/parallax-slider/modernizr.custom.28468.js">
+    </script>
+    
+    
+    
+    
+    
+    
+    <script type="text/javascript">
+
+		  function checkForm(form)
+		  {
+
+		    if(!form.captcha.value.match(/^\d{5}$/)) {
+		      alert('Please enter the CAPTCHA digits in the box provided');
+		      form.captcha.focus();
+		      return false;
+		    }
+
+		    return true;
+		  }
+
+</script>
+
+  </head>
+
+  <body>
+    <!--header start-->
+    <script type="text/javascript" src="js/header.js">  </script>
+    <!--header end-->
+
+
+
+  <!--breadcrumbs start-->
+    <div class="breadcrumbsDonatePg">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-4 col-sm-4">
+            <h1>
+              Contact
+            </h1>
+          </div>
+          <div class="col-lg-8 col-sm-8">
+            <ol class="breadcrumb pull-right">
+              <li>
+                <a href="Contact.php#">
+                  Home
+                </a>
+              </li>
+              
+              <li class="active">
+                Contact
+              </li>
+            </ol>
+          </div>
+        </div>
+      </div>
+      
+    </div>
+    <!--breadcrumbs end-->
+
+
+    <!--container start-->
+    <div class="donateBkg">
+<br>&nbsp<br>
+		    <div class="container registration-bg-white" style="padding: 50px;">
+
+
+		      <div class="row">
+		        <div class="col-lg-5 col-sm-5 address">
+		          <section class="contact-infos">
+		            <h4 class="title custom-font text-black">
+		              OFFICE ADDRESS
+		            </h4>
+		            <address>
+		              #14, AJR Pride, 2nd Floor,
+		              <br>
+						27th Main, Sector 1,
+						<br>
+						HSR Layout,  Bangalore-560102
+		            </address>
+		          </section>
+		          
+		          
+           		<section class="contact-infos">
+		            <h4>
+		              EMAIL
+		            </h4>
+		            <p>
+		              <i class="icon-phone">
+		              </i>
+		              info@saytrees.org
+		            </p>
+		
+		            <p>
+		              <i class="icon-phone">
+		              </i>
+		              
+		            </p>
+		
+		          </section>
+        
+		          
+		          <section class="contact-infos">
+		            <h4>
+		              TELEPHONE
+		            </h4>
+		            <p>
+		              <i class="icon-phone">
+		              </i>
+		              +91-9008659007
+		            </p>
+		
+		           
+		
+		          </section>
+		        </div>
+		        <div class="col-lg-7 col-sm-7 address">
+		          <h4>
+		            Drop us a line so that we can hear from you
+		          </h4>
+		          
+		            
+		          		          
+		          
+		          <div class="contact-form">
+		            <form method="post" onsubmit="return checkForm(this);">
+		              <div class="form-group">
+		                <label for="name">
+		                  Name
+		                </label>
+		                <input type="text" placeholder="" id="name" name="name" required="required" class="form-control">
+		              </div>
+		              <div class="form-group">
+		                <label for="email">
+		                  Email
+		                </label>
+		                <input type="text" placeholder="" id="email" name="email" required="required" class="form-control">
+		              </div>
+		              <div class="form-group">
+		                <label for="phone">
+		                  Phone
+		                </label>
+		                <input type="text" id="phone" class="form-control" name="phone" required="required">
+		              </div>
+		              <div class="form-group">
+		                <label for="city">
+		                  City
+		                </label>
+		                <input type="text" id="city" class="form-control" name="city" required="required">
+		              </div>
+		              
+		              <div class="form-group">
+		                <label for="city">
+		                  Your Interest
+		                </label>
+		                <label class="checkbox">
+                        <input type="checkbox"  name="ContactSubect"  value="SAYTREES - GENERAL ENQUIRY"> I wish to plant saplings <br>
+                        <input type="checkbox"  name="ContactSubect"  value="SAYTREES - REGULAR VOLUNTEER"> I wish to volunteer regularly  <br>
+                        <input type="checkbox"  name="ContactSubect"  value="SAYTREES - CORPORATE PLANTATION"> My company wishes to plant saplings as part of CSR <br>
+                        <input type="checkbox"  name="ContactSubect"  value="SAYTREES - DONATE"> I wish to donate for tree plantation  <br>
+                        <input type="checkbox"  name="ContactSubect"  value="SAYTREES - OCASSSION PLANTATION">  I wish to plant saplings on my birthday/anniversary/rememberance etc  <br>
+                        <input type="checkbox"  name="ContactSubect"  value="SAYTREES - CREATIVE HELP">  I would like to help with my creativity  <br>
+                        <input type="checkbox"  name="ContactSubect"  value="SAYTREES - OTHER"> Others <br>
+                        
+                    	</label>
+		              </div>
+		              
+		               
+		              
+		              
+		              
+		              
+		              <div class="form-group">
+		                <label for="phone">
+		                  Message
+		                </label>
+		                <textarea name="message" placeholder="" rows="5" class="form-control">
+		                </textarea>
+		              </div>
+		              
+		              
+		              <div class="form-group">
+		                <label for="phone">
+		                  Just making sure you are not a robot !
+		                </label>
+		                <img src="captcha/captcha.php" width="160" height="45" border="1" alt="CAPTCHA">
+		                <input type="text" id="captcha" class="form-control" name="captcha" required="required" placeholder="Please enter the text seen above">
+		              </div>
+		              
+		              
+		              <button class="btn btn-info" type="submit">
+		                Submit
+		              </button>
+		            </form>
+		
+		          </div>
+		          
+		          		          
+		        </div>
+		      </div>
+
+    </div>
+
+      
+	</div>
+
+
+
+    <!--container end-->
+
+    <!--footer start-->
+    <script type="text/javascript" src="js/footer.js">  </script>
+    <!--small footer end-->
+    
+    
+    
+   	<script src="js/jquery-1.8.3.min.js">
+    </script>
+    <script src="js/bootstrap.min.js">
+    </script>
+   
+    
+   
+
+    
+    <script src="js/wow.min.js">
+    </script>
+   
+
+   
+    
+    
+    
+    
+
+
+
+    <!--common script for all pages-->
+    <script src="js/common-scripts.js">
+    </script>
+
+   
+
+
+    <script>
+      $('a.info').tooltip();
+
+      $(window).load(function() {
+        $('.flexslider').flexslider({
+          animation: "slide",
+          start: function(slider) {
+            $('body').removeClass('loading');
+          }
+        });
+      });
+
+      $(document).ready(function() {
+
+        $("#owl-demo").owlCarousel({
+
+          items : 4
+
+        });
+
+      });
+
+      jQuery(document).ready(function(){
+        jQuery('ul.superfish').superfish();
+      });
+
+      new WOW().init();
+
+
+    </script>
+  </body>
+</html>
+ 
---
+++ Donate.php
@@ -0,0 +1,359 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="description" content="We are actively involved in tree plantation to increase the green cover. We have been planting trees in Bangalore since years. This is just a small step to save the oxygen factories around us. These tree plantation drives in Bangalore spreads awareness among citizens and makes them understand the importance of trees around us." />
+<meta name="keywords" content="tree plant, plant tree, save tree, plant tree in bangalore, save tree in bangalore, tree plantation in bangalore, tree plantatin in india, save trees india, save trees, bangalore tree, tree planting group,  plant tree in Hyderabad, save tree in Hyderabad, tree plantation in Hyderabad, Hyderabad tree, Hyderabad tree planting,save tree in delhi, tree plantation in delhi, delhi tree, delhi tree planting,save tree in pune, tree plantation in pune, pune tree, pune tree planting,save tree in chennai, tree plantation in chennai, chennai tree, chennai tree planting" />
+<title>Say Trees</title>
+<link rel="shortcut icon" href="http://www.saytrees.org/images/icons/tabIcon.png"/> 
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/theme.css" rel="stylesheet">
+    <link href="css/bootstrap-reset.css" rel="stylesheet">
+    <!-- <link href="css/bootstrap.min.css" rel="stylesheet">-->
+
+    <!--external css-->
+    <link href="assets/font-awesome/css/font-awesome.css" rel="stylesheet" />
+    <link rel="stylesheet" href="css/flexslider.css"/>
+    <link href="assets/bxslider/jquery.bxslider.css" rel="stylesheet" />
+    <link rel="stylesheet" href="css/animate.css">
+    <link rel="stylesheet" href="assets/owlcarousel/owl.carousel.css">
+    <link rel="stylesheet" href="assets/owlcarousel/owl.theme.css">
+
+    <link href="css/superfish.css" rel="stylesheet" media="screen">
+    <link href='http://fonts.googleapis.com/css?family=Lato' rel='stylesheet' type='text/css'>
+    
+
+    <!-- Custom styles for this template -->
+    <link rel="stylesheet" type="text/css" href="css/component.css">
+    <link href="css/style.css" rel="stylesheet">
+    <link href="css/style-responsive.css" rel="stylesheet" />
+
+    <link rel="stylesheet" type="text/css" href="css/parallax-slider/parallax-slider.css" />
+    <script type="text/javascript" src="js/parallax-slider/modernizr.custom.28468.js">
+    </script>
+    
+    
+    
+    <script>
+    var hash = '';
+	
+    function submitPayuForm() {
+      if(hash == '') {
+        return;
+      }
+      var payuForm = document.forms.payuForm;
+      payuForm.submit();
+    }
+  </script>
+    
+
+  </head>
+
+  <body id="Donate"  onload="submitPayuForm()">
+    <!--header start-->
+    <script type="text/javascript" src="js/header.js">  </script>
+    <!--header end-->
+
+
+
+  <!--breadcrumbs start-->
+    <div class="breadcrumbsDonatePg">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-4 col-sm-4">
+            <h1>
+              Donate
+            </h1>
+          </div>
+          <div class="col-lg-8 col-sm-8">
+            <ol class="breadcrumb pull-right">
+              <li>
+                <a href="Donate.php#">
+                  Home
+                </a>
+              </li>
+              
+              <li class="active">
+                Donate
+              </li>
+            </ol>
+          </div>
+        </div>
+      </div>
+      
+    </div>
+    <!--breadcrumbs end-->
+
+
+    <!--container start-->
+    <div class="fixedBkg">
+
+		<div class="container">
+		
+			<div>
+				<div class="col-lg-12">
+				&nbsp;
+				</div>
+			</div>
+		
+		
+			<div class="row">
+				<div class="col-lg-12">
+					<div class="col-md-1 col-sm-1 ">
+						
+						&nbsp;
+						
+					
+					</div>
+					<div class="col-md-5 col-sm-5 ">
+						<div class="blog-left wow fadeInUp">
+							<div style="height: 540px;">
+								    <div class="registration-bg">
+								        <div class="container"  >
+								        
+								        
+									         											    <form action="Donate.php" method="post" name="payuForm" class="form-signin wow fadeInUp">
+											      <input type="hidden" name="key" value="f9w641" />
+											      <input type="hidden" name="hash" value=""/>
+											      <input type="hidden" name="txnid" value="b96acb51669d2e2d2e7d" />
+												  <input type="hidden" name="surl" value="http://www.saytrees.org/PaymentSuccess.html" />
+											      <input type="hidden" name="furl" value="http://www.saytrees.org/PaymentFailure.html"/>
+											      <input type="hidden" name="service_provider" value="payu_paisa" />
+											      <input type="hidden" name="productinfo" value="Contribution for saplings"/>
+											     
+									        
+								        
+								        
+								        
+								        
+								
+								           
+								                <h2 class="form-signin-heading">Donate for a clean air</h2>
+								                <div class="login-wrap">
+								                    <p>Enter personal details</p>
+								                    <input type="text" class="form-control" placeholder="Full Name" autofocus="" name="firstname" id="firstname" value="">
+								                    <input type="text" class="form-control" placeholder="Email Id" autofocus="" name="email" id="email"  value="" >
+								                    <input type="text" class="form-control" placeholder="Contact Number" autofocus="" name="phone"    value="">
+								                    
+								
+								                    <p> Your Contribution</p>
+								                    <input name="amount" type="text" class="form-control" placeholder="Amount" autofocus="" value="">
+								                    
+								                    								                    <button class="btn btn-lg btn-login btn-block" type="submit">Submit</button>
+																					                    
+								                </div>
+								            </form>
+								
+								        </div>
+								     </div>
+							</div>
+						</div>
+					
+					</div>
+					<div class="col-md-5 col-sm-5 ">
+						<div class="blog-left wow fadeInUp">
+							<div style="height: 540px;">
+								    <div class="registration-bg">
+								        <div class="container"  >
+								
+								            <form class="form-signin wow fadeInUp" action="Donate.php">
+								                <h2 class="form-signin-heading">Your contribution helps</h2>
+								                <div class="login-wrap">
+								                     <p>
+
+
+														You can also transfer your contribution to SAYTREES' bank account.<br>
+											<b>SAYTREES ENVIRONMENTAL TRUST</b><br>
+											Current Account.<br>
+											Account # 34803899434<br>
+											State Bank Of India, Hosur Road Branch, Bangalore - 560100<br>
+											IFSC code - SBIN0010514<br>
+											MICR Code - 560002108<br><br>
+											Write to us at <b>info@saytrees.org</b> after transferring your contribution <br><br>
+											
+											SayTrees is <b>FCRA certified and can</b> receive donations from any country. FCRA bank details will be shared upon request<br>
+			
+													</p>
+								                    
+								                    
+								                    
+								                </div>
+								            </form>
+								
+								        </div>
+								     </div>
+							</div>
+						</div>
+					
+					</div>
+					
+					<div class="col-md-1 col-sm-1 ">
+						
+						&nbsp;
+						
+					
+					</div>
+				</div>
+			</div>
+			<br>&nbsp;<br>&nbsp;
+		</div>
+      
+      
+      
+      
+      
+      <div class="white-bg">
+      	<div class="container">
+        	<div class="row">
+          		<div class="col-lg-12 recent">
+		          	<div class="col-md-12 col-sm-12 " style="text-align: center;">
+		          		<br>
+		          		<img src="images/donate-heading.jpg" />
+		          	</div>
+          		</div>
+         	</div>
+         	<div class="row">
+          		<div class="col-lg-12 recent">
+		          	<div class="col-md-1 col-sm-1 " style="text-align: center;"></div>
+		          	<div class="col-md-5 col-sm-5 " style="text-align: center;">
+		          		<img src="images/donate-city.jpg" />
+		          	</div>
+		          	<div class="col-md-5 col-sm-5 " style="text-align: center;">
+		          		<img src="images/donate-city-text.jpg" />
+		          	</div>
+		          	<div class="col-md-1 col-sm-1 " style="text-align: center;"></div>
+          		</div>
+         	</div>
+         	<div class="row">
+          		<div class="col-lg-12 recent">
+		          	<div class="col-md-12 col-sm-12 " style="text-align: center;">
+		          		<br>
+		          		&nbsp;
+		          		<br>
+		          		&nbsp;
+		          	</div>
+          		</div>
+         	</div>
+         	<div class="row">
+          		<div class="col-lg-12 recent">
+		          	<div class="col-md-1 col-sm-1 " style="text-align: center;"></div>
+		          	<div class="col-md-5 col-sm-5 " style="text-align: center;">
+		          		<img src="images/donate-village-text.jpg" />
+		          	</div>
+		          	<div class="col-md-5 col-sm-5 " style="text-align: center;">
+		          		<img src="images/donate-village.jpg" />
+		          	</div>
+		          	<div class="col-md-1 col-sm-1 " style="text-align: center;"></div>
+          		</div>
+         	</div>
+         	<div class="row">
+          		<div class="col-lg-12 recent">
+		          	<div class="col-md-12 col-sm-12 " style="text-align: center;">
+		          		<br>
+		          		&nbsp;
+		          		<br>
+		          		&nbsp;
+		          	</div>
+          		</div>
+         	</div>
+         	<div class="row">
+          		<div class="col-lg-12 recent">
+		          	<div class="col-md-1 col-sm-1 " style="text-align: center;"></div>
+		          	<div class="col-md-5 col-sm-5 " style="text-align: center;">
+		          		<img src="images/donate-forest.jpg" />
+		          	</div>
+		          	<div class="col-md-5 col-sm-5 " style="text-align: center;">
+		          		<img src="images/donate-forest-text.jpg" />
+		          	</div>
+		          	<div class="col-md-1 col-sm-1 " style="text-align: center;"></div>
+          		</div>
+         	</div>
+         	
+       	</div>
+  	</div>
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+	</div>
+
+
+
+    <!--container end-->
+
+    <!--footer start-->
+    <script type="text/javascript" src="js/footer.js">  </script>
+    <!--small footer end-->
+    
+    
+    
+   	<script src="js/jquery-1.8.3.min.js">
+    </script>
+    <script src="js/bootstrap.min.js">
+    </script>
+   
+    
+   
+
+    
+    <script src="js/wow.min.js">
+    </script>
+   
+
+   
+    
+    
+    
+    
+
+
+
+    <!--common script for all pages-->
+    <script src="js/common-scripts.js">
+    </script>
+
+   
+
+
+    <script>
+      $('a.info').tooltip();
+
+      $(window).load(function() {
+        $('.flexslider').flexslider({
+          animation: "slide",
+          start: function(slider) {
+            $('body').removeClass('loading');
+          }
+        });
+      });
+
+      $(document).ready(function() {
+
+        $("#owl-demo").owlCarousel({
+
+          items : 4
+
+        });
+
+      });
+
+      jQuery(document).ready(function(){
+        jQuery('ul.superfish').superfish();
+      });
+
+      new WOW().init();
+
+
+    </script>
+  </body>
+</html>
